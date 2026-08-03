482af35dd4c5817d5050488cd629c3600daca74b
Francesco Cassese <francescocassese9@gmail.com>
2026-08-03 19:16:32 +0200

    feat: add hover overlay with synopsis to comic card

--- resources/views/components/comic-card.blade.php
+++ resources/views/components/comic-card.blade.php
@@ -1,10 +1,15 @@
 @props(['comic'])
 
-<div class="card comic-card h-100">
+<div class="card comic-card h-100" tabindex="0">
     <div class="comic-thumb-wrapper">
         <img src="{{ $comic['thumb'] }}" class="card-img-top comic-thumb" alt="{{ $comic['title'] }}" loading="lazy">
         @if (!empty($comic['series']))
             <span class="comic-series-badge">{{ $comic['series'] }}</span>
+        @endif
+        @if (!empty($comic['description']))
+            <div class="comic-card-overlay">
+                <p class="comic-card-synopsis">{{ \Illuminate\Support\Str::limit($comic['description'], 140) }}</p>
+            </div>
         @endif
     </div>
     <div class="card-body d-flex flex-column">
