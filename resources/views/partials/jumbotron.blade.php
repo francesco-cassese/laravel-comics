54c0335dc4bc0c6da0a88460c1dbf16db6309793
Francesco Cassese <francescocassese9@gmail.com>
2026-08-03 18:50:00 +0200

    feat: add hero jumbotron section with background image

---
+++ resources/views/partials/jumbotron.blade.php
@@ -0,0 +1,7 @@
+<div class="jumbotron-container">
+    <div class="jumbotron-content">
+        <h1 class="jumbotron-title">Scopri il mondo <span class="jumbotron-title-accent">Comics</span></h1>
+        <p class="jumbotron-subtitle">Tutte le nuove uscite, i classici intramontabili e le action figures più esclusive. Tuffati nell'universo dei tuoi supereroi preferiti.</p>
+        <a href="{{ route('fumetti') }}" class="jumbotron-btn">Esplora il catalogo</a>
+    </div>
+</div>
