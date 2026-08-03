d1c5f83505d8ed02fb1ea74ccaeaf4ff7adbba49
Francesco Cassese <francescocassese9@gmail.com>
2026-08-03 19:49:47 +0200

    refactor: switch to Bootstrap Sass source for variable overrides

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Comics Shop')</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
 <body>
